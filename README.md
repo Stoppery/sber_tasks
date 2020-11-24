54d8ebd22fd3b437585bb4d0aa3a82121b72b4e6
Maxim <max.miromanov@yandex.ru>
2020-11-24 07:26:32 +0300

    done

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 
 ## Repo contents
-1. [First task - count numbers](https://github.com/Stoppery/sber_tasks/blob/master/first_task.js) 				||       Result amount: 55252
+1. [First task - count numbers](https://github.com/Stoppery/sber_tasks/blob/master/first_task.js) 				||       Result amount: 55252 (Also added modified version wich works with 1 && 2 && 3 combinations)
 2. [Second task - parse json and push all data to DataBase (PostgreSQL)](https://github.com/Stoppery/sber_tasks/tree/master/second_task)
---
+++ first_task_v2.js
@@ -0,0 +1,31 @@
+// function also works with comb = 2 || 1 || 3
+function amountOfNumbers() {
+	let base = 10, comb = 3, res=0, coef=0, f = 10;
+
+	for (let x = 0; x < comb * 9 + 1 ; x++) {
+		if (x < base) {
+			coef = 1;
+			for (let m = 1; m < comb; m++) {
+				console.log('1');
+				coef *= (x + m);
+			}
+			coef /= (comb != 1) ? (comb-1) : 1; 
+		} else if ((comb * 9 + 1) - x <= base){
+			coef = 1;
+			for (let m = 0; m < comb-1; m++) {
+				console.log('1');
+				coef *= ((comb * 9 + 1) - x + m);
+			}
+			coef /= (comb != 1) ? (comb-1) : 1; 
+		} else  {
+			if (comb == 3){
+				f -= 2;
+				coef += f;
+			}
+		}
+		res += coef ** 2;
+	}
+	return res;
+}
+
+console.log(amountOfNumbers());
